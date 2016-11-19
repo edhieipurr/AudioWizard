a373ca322160727a6890137e119fe6eaa90e868e
Aleksandr Romanov <samroproduction@gmail.com>
2016-11-19 17:56:25 +0200

    Add SDK 23 permission handling in runtime

--- app/src/main/java/com/example/aleksandrromanov/audiowizard/RecordAudio.java
+++ app/src/main/java/com/example/aleksandrromanov/audiowizard/RecordAudio.java
@@ -1,10 +1,17 @@
 package com.example.aleksandrromanov.audiowizard;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.media.PlaybackParams;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,12 +27,19 @@ public class RecordAudio extends AppCompatActivity {
     private static final String LOG_TAG = "RecordAudio";
     private MediaRecorder recorder;
     MediaPlayer mediaPlayer;
+    private String[] perms = {"android.permission.RECORD_AUDIO", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private static int MY_PERMISSIONS_REQUEST = 200;
+
 
     private void ConfigureMediaRecorder(MediaRecorder recorder){
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.3gp");
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try{
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.3gp");
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        }catch (Exception e){
+            Log.e(LOG_TAG, e.toString());
+        }
     }
 
     private void PlayRecorderAudio(MediaPlayer mediaPlayer){
@@ -37,15 +51,27 @@ public class RecordAudio extends AppCompatActivity {
         }catch (Exception e){
             Log.e(LOG_TAG, e.toString());
         }
-
-
     }
 
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
+        switch(permsRequestCode){
+            case 200:
+                boolean audioAccepted = grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                boolean externalStorageAccepted = grantResults[1]==PackageManager.PERMISSION_GRANTED;
+                if(audioAccepted && externalStorageAccepted){
 
+                    ConfigureMediaRecorder(recorder);
+                }
+                break;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestPermissions(perms, MY_PERMISSIONS_REQUEST);
         recorder = new MediaRecorder();
         mediaPlayer = new MediaPlayer();
         //Debugging toast item
@@ -54,14 +80,7 @@ public class RecordAudio extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         text = Environment.getExternalStorageDirectory().getAbsolutePath();
         final Toast toast = Toast.makeText(context, text, duration);
-        //Problem with sdk 23 Runtime permissions (switched to an old target SDK (22) TO include manifest permissions statically)
-
-       // if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-         //   Log.e(LOG_TAG,"Permission Denied");
-        //}
-        ConfigureMediaRecorder(recorder);
         setContentView(R.layout.activity_record_audio);
-
         final Button record = (Button) this.findViewById(R.id.record_button);
         final Button stop = (Button) this.findViewById(R.id.stop_button);
         final Button play = (Button) this.findViewById(R.id.play_button);
@@ -73,7 +92,6 @@ public class RecordAudio extends AppCompatActivity {
                 stop.setEnabled(true);
 
                     try {
-                        //mediaPlayer.reset();
                         recorder.prepare();
                         recorder.start();
 
