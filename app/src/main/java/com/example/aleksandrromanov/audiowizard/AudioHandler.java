1dd22d1c8329b559a27bb13710543f40b39eadad
Aleksandr Romanov <samroproduction@gmail.com>
2016-12-06 21:12:05 +0200

    Add other filter functionalities (Beta)

--- app/src/main/java/com/example/aleksandrromanov/audiowizard/AudioHandler.java
+++ app/src/main/java/com/example/aleksandrromanov/audiowizard/AudioHandler.java
@@ -2,8 +2,11 @@ package com.example.aleksandrromanov.audiowizard;
 
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.media.PlaybackParams;
 import android.os.Environment;
 import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * Created by aleksandrromanov on 02/12/2016.
@@ -11,6 +14,7 @@ import android.util.Log;
 
 class AudioHandler {
 
+    private PlaybackParams params = new PlaybackParams();
     private static final String LOG_TAG = "AudioHandler";
     protected String pathToMediaFile = Environment.getExternalStorageDirectory().getAbsolutePath()+"/audio.mp4";
 
@@ -42,8 +46,74 @@ class AudioHandler {
             mediaPlayer.setDataSource(pathToMediaFile);
             mediaPlayer.prepare();
             mediaPlayer.start();
+
         }catch (Exception e){
             Log.e(LOG_TAG, e.toString());
+
+        }
+    }
+
+    protected void PlayVaderSound(){
+
+        try{
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(pathToMediaFile);
+            //ADD VADER SOUND
+            mediaPlayer.setPlaybackParams(params.setPitch(0.65f));
+            mediaPlayer.setPlaybackParams(params.setSpeed(0.5f));
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        }catch (Exception e){
+            Log.e(LOG_TAG, e.toString());
+
+        }
+    }
+
+    protected void PlayHamsterSound(){
+
+        try{
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(pathToMediaFile);
+            mediaPlayer.setPlaybackParams(params.setPitch(2.0f));
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        }catch (Exception e){
+            Log.e(LOG_TAG, e.toString());
+
+        }
+    }
+
+    protected void PlaySnailSound(){
+
+        try{
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(pathToMediaFile);
+            //mediaPlayer.setPlaybackParams(params.setPitch(1.0f));
+            mediaPlayer.setPlaybackParams(params.setSpeed(0.3f));
+
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        }catch (Exception e){
+            Log.e(LOG_TAG, e.toString());
+
+        }
+    }
+
+    protected void PlayReverseSound(){
+
+        try{
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(pathToMediaFile);
+            mediaPlayer.setPlaybackParams(params.setPitch(10f));
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        }catch (Exception e){
+            Log.e(LOG_TAG, e.toString());
+
         }
     }
 
--- app/src/main/java/com/example/aleksandrromanov/audiowizard/SoundFilterActivity.java
+++ app/src/main/java/com/example/aleksandrromanov/audiowizard/SoundFilterActivity.java
@@ -2,6 +2,7 @@ package com.example.aleksandrromanov.audiowizard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,24 +11,48 @@ import android.widget.ImageView;
 public class SoundFilterActivity extends AppCompatActivity {
 
     private AudioHandler audioHandler;
-
-
+    private static final String LOG_TAG = "SoundFilterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_filter);
         final ImageView darth = (ImageView) this.findViewById(R.id.vader);
+        final ImageView snail = (ImageView) this.findViewById(R.id.snail);
+        final ImageView reverse = (ImageView) this.findViewById(R.id.reverse);
+        final ImageView hamster = (ImageView) this.findViewById(R.id.hamster);
         audioHandler = new AudioHandler();
         darth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                audioHandler.PlayRecorderAudio();
-
+                audioHandler.PlayVaderSound();
+                Log.v(LOG_TAG,"VADER");
             }
         });
-        
+
+        hamster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioHandler.PlayHamsterSound();
+                Log.v(LOG_TAG,"HAMSTER");
+            }
+        });
+
+        reverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioHandler.PlayReverseSound();
+                Log.v(LOG_TAG,"REVERSE");
+            }
+        });
+
+        snail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioHandler.PlaySnailSound();
+                Log.v(LOG_TAG,"SNAIL");
+            }
+        });
 
 
     }
